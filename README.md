f87a5e40b339e8ac088994f0bb5b90e92d98eceb
Johnny <john.arena.3@gmail.com>
2016-04-25 19:52:35 -0500

    rebase
    Signed-off-by: Johnny <john.arena.3@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Overwatch-watch
+
+Watch Overwatch.
+Spits out a list of twitch streamers streaming Overwatch. Styled to look like an overwatch thing.
+Go hog wild!
---
+++ oww.js
@@ -0,0 +1,24 @@
+var owwApp = angular.module('owwApp', []);
+
+owwApp.factory('dataService', function($http) {
+  var myService = {
+    async: function() {
+      var promise = $http.get('https://api.twitch.tv/kraken/streams?game=overwatch').then(function (response)
+      {
+        return response.data;
+      });
+      return promise;
+    }
+  };
+  return myService;
+});
+
+owwApp.controller('StreamersController', ['$scope', 'dataService', function($scope, dataService)
+{
+  var streamers = this;
+  streamers.list ={};
+  dataService.async().then(function(d) {
+    $scope.data = d;
+    streamers.list = d.streams;
+  });
+}]);
